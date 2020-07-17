5d1834dab665ba3cfcdbec4811f5c50c56f57153
mashrur ayon <40263721+mashrur-ayon@users.noreply.github.com>
2020-07-17 18:14:44 +0600

    plot 5

---
+++ plot5.R
@@ -0,0 +1,28 @@
+library("data.table")
+library("ggplot2")
+
+
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+# Loading the data.
+NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
+SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
+
+# subsetting
+vehiclesSCC <- SCC[grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+                   , SCC]
+vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+
+# subsetting
+baltimoreVehiclesNEI <- vehiclesNEI[fips=="24510",]
+
+png("plot5.png")
+
+ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity", fill ="#FF9999" ,width=0.75) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+
+dev.off()
